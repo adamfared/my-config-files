a1360ea1bf601ee2b2466a7b7ed4a2d5987bff54
adamfared <adamfared100@gmail.com>
2026-08-31 00:03:57 +0300

    added config file

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
+if status is-login; and test -z "$DISPLAY"; and test (tty) = "/dev/tty1"
+    exec mango
+end
+
 if status is-interactive
     fastfetch
 end
 
 alias clock="tty-clock -c -C 1 -t"
 alias matrix="cmatrix -C red -b"
+alias lava="lavat -c red"
