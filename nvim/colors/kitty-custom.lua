8ec6516b03054c085edbd89002a28a8164d2b00c
adamfared <adamfared100@gmail.com>
2026-08-30 22:34:58 +0300

    edited config file

--- nvim/colors/kitty-custom.lua
+++ nvim/colors/kitty-custom.lua
@@ -33,7 +33,7 @@ hl("CursorLine", { bg = "NONE" })
 hl("CursorLineNr", { fg = theme.red_light, bold = true })
 hl("Visual", { bg = theme.selection, fg = "#000000" })
 
--- --- Dashboard & Icons (Light Red Overrides) ---
+-- --- Dashboard & Icons ---
 hl("NvimTreeFolderIcon", { fg = theme.red_light })
 hl("NvimTreeFileIcon", { fg = theme.red_light })
 hl("MiniIconsRed", { fg = theme.red_light })
@@ -50,7 +50,7 @@ hl("SnacksDashboardKey", { fg = theme.red_light })
 hl("SnacksDashboardDesc", { fg = theme.fg })
 hl("SnacksDashboardFooter", { fg = theme.grey, italic = true })
 
--- --- Telescope & Pickers (Light Red) ---
+-- --- Telescope & Pickers ---
 hl("TelescopeNormal", { fg = theme.fg, bg = theme.bg })
 hl("TelescopeBorder", { fg = theme.red_light, bg = theme.bg })
 hl("TelescopePromptBorder", { fg = theme.red_light, bg = theme.bg })
@@ -62,31 +62,46 @@ hl("TelescopePreviewTitle", { fg = theme.red_light, bold = true })
 hl("TelescopePromptTitle", { fg = theme.red_light, bold = true })
 hl("TelescopeResultsTitle", { fg = theme.red_light, bold = true })
 
--- --- Status Bar & Mode Indicator (Light Red Bottom Bar) ---
-hl("StatusLine", { fg = theme.fg, bg = theme.bg })
-hl("StatusLineNC", { fg = theme.grey, bg = theme.bg })
-
--- Lualine / Status modes
-hl("lualine_a_normal", { bg = theme.red_light, fg = "#000000", bold = true })
-hl("lualine_a_insert", { bg = theme.red_light, fg = "#000000", bold = true })
-hl("lualine_a_visual", { bg = theme.red_light, fg = "#000000", bold = true })
-hl("lualine_a_replace", { bg = theme.red_light, fg = "#000000", bold = true })
-hl("lualine_a_command", { bg = theme.red_light, fg = "#000000", bold = true })
-hl("lualine_b_normal", { fg = theme.red_light, bg = theme.bg })
-hl("lualine_c_normal", { fg = theme.fg, bg = theme.bg })
-
--- --- Code Syntax ---
+-- --- Code Syntax & Treesitter ---
 hl("Keyword", { fg = theme.red, bold = true })
 hl("Statement", { fg = theme.red })
 hl("Function", { fg = theme.magenta })
 hl("String", { fg = theme.green })
+hl("Boolean", { fg = theme.yellow })
+hl("Number", { fg = theme.yellow })
 hl("Constant", { fg = theme.yellow })
 hl("Comment", { fg = theme.grey, italic = true })
 hl("Identifier", { fg = theme.fg })
 
--- Treesitter
 hl("@keyword", { fg = theme.red, bold = true })
+hl("@keyword.import", { fg = theme.red_light, bold = true })
 hl("@function", { fg = theme.magenta })
+hl("@function.builtin", { fg = theme.magenta })
 hl("@string", { fg = theme.green })
+hl("@string.path", { fg = theme.green, underline = true })
+hl("@boolean", { fg = theme.yellow })
+hl("@number", { fg = theme.yellow })
 hl("@variable", { fg = theme.fg })
+hl("@variable.member", { fg = theme.red_light })
+hl("@variable.parameter", { fg = theme.red_light })
+hl("@property", { fg = theme.red_light })
 hl("@comment", { fg = theme.grey, italic = true })
+hl("@punctuation.delimiter", { fg = theme.red_light })
+hl("@punctuation.bracket", { fg = theme.fg })
+hl("@operator", { fg = theme.red })
+-- --- Fully Transparent Statusline Overrides ---
+hl("StatusLine", { fg = theme.fg, bg = "NONE" })
+hl("StatusLineNC", { fg = theme.grey, bg = "NONE" })
+
+-- Force transparency on Lualine background containers
+hl("lualine_c_normal", { fg = theme.fg, bg = "NONE" })
+hl("lualine_c_inactive", { fg = theme.grey, bg = "NONE" })
+hl("lualine_b_normal", { fg = theme.red_light, bg = "NONE" })
+hl("lualine_b_insert", { fg = theme.red_light, bg = "NONE" })
+hl("lualine_b_visual", { fg = theme.red_light, bg = "NONE" })
+hl("lualine_x_normal", { fg = theme.red_light, bg = "NONE" })
+hl("lualine_y_normal", { fg = theme.red_light, bg = "NONE" })
+
+-- Transition separators / powerline glyphs
+hl("lualine_transitional_lualine_a_normal_to_lualine_b_normal", { fg = theme.red_light, bg = "NONE" })
+hl("lualine_transitional_lualine_b_normal_to_lualine_c_normal", { fg = theme.red_light, bg = "NONE" })
